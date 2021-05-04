db759994a0ad2f9009caa4c21b358ea46791c5e7
divya212109 <73348305+divya212109@users.noreply.github.com>
2021-05-04 18:09:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+song = "";
+scoreleftWrist = 0;
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+
+
+
+function setup() {
+
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+    fill("#799c9c");
+    stroke("#739c5d");
+if(scoreleftWrist > 0.2)
+{
+
+
+    circle(leftWristX, leftWristY, 20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals / 500;
+    document.getElementById("volume").innerHTML = "volume = " + volume;
+    song.setVolume(volume);
+}}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized')
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY" + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY" + rightWristY);
+    }
+}
+
